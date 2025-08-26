ed6591f39714e8e412bbccc8fd4cb7b1b7aef98e
David Plowman <david.plowman@raspberrypi.com>
2025-08-26 09:39:54 +0100

    ipa: rpi: Support memory cameras
    Add support for "memory cameras" (for Bayer reprocessing).
    
    Mostly very minor changes, but the way we fill in certain bits of
    metadata before running the IPAs changes significantly (see
    prepareIspFillMetadata).
    
    For example, regular sensors get device status information from the
    the sensor (or possibly from values we wrote to the sensor a few
    frames back), whereas "memory cameras" assume that the exposure/gain
    values passed in with the request controls will be the ones for
    setting up the tuning algorithms.
    
    Signed-off-by: David Plowman <david.plowman@raspberrypi.com>

--- src/ipa/rpi/common/ipa_base.h
+++ src/ipa/rpi/common/ipa_base.h
@@ -45,6 +45,10 @@ public:
 	void mapBuffers(const std::vector<IPABuffer> &buffers) override;
 	void unmapBuffers(const std::vector<unsigned int> &ids) override;
 
+	void prepareIspFillMetadata(const PrepareParams &params, unsigned int ipaContext,
+				    Span<uint8_t> &embeddedBuffer, bool &hdrChange,
+				    bool &delayedRequestControls);
+
 	void prepareIsp(const PrepareParams &params) override;
 	void processStats(const ProcessParams &params) override;
 
@@ -142,6 +146,8 @@ private:
 		int32_t mode;
 		utils::Duration manualPeriod;
 	} flickerState_;
+
+	bool isMemoryCamera_;
 
 	bool cnnEnableInputTensor_;
 	bool awbEnabled_;
